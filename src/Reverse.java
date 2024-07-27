b36af36c76de1d9fe37211cfeb864e116f670e3d
Divyakamal-004 <kamaldivya650@gmail.com>
2024-07-27 08:26:12 +0530

    reversing of number

---
+++ src/Reverse.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Reverse {
+    public static void main(String[] args) {
+        Scanner inp=new Scanner(System.in);
+        int n=inp.nextInt();
+       rev(n);
+
+    }
+    static void rev (int n){
+        if(n==n%10){
+            System.out.print(n);
+        }
+       else{
+           System.out.print(n%10);
+           rev(n/10);
+        }
+    }
+
+    }
+
